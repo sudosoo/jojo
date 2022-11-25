bc5dbba4ce8dd1164dd7409cd0803c10049973a1
antidote <ljh8651@nate.com>
2022-11-25 21:55:35 +0900

    11.25 final

---
+++ src/main/java/bank/Bank.java
@@ -0,0 +1,42 @@
+package bank;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Bank {
+    public LocalDateTime time;
+    public String customerName;
+    public String customerAccount;
+    public int customerBalance;
+    public String bankName;
+    public String check;
+
+
+    public Bank(String customerName, String customerTotal, int customerBalance, String bankName,String check,LocalDateTime time) {
+        this.customerName = customerName;
+        this.customerAccount = customerTotal;
+        this.customerBalance = customerBalance;
+        this.bankName = bankName;
+        this.check= check;
+        this.time = LocalDateTime.now();
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        customerName = customerName;
+    }
+    public String getCustomerAccount() {
+        return customerAccount;
+    }
+
+
+    @Override
+    public String toString() {
+        return "계좌 { 은행명: " + bankName+ ", 이름: " + customerName
+                + ", 계좌번호: " + customerAccount.substring(0, 4) + "-" + customerAccount.substring(4, 8) + "-" + customerAccount.substring(8, 12)
+                + ", 계좌잔고: " + customerBalance +" "+ check +"시간 :"+time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd/HH시mm분ss초"))+" }";
+    }
+}
+
+
